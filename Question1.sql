9357d44eb317ab744cf1ba5b48734b1b82a0baff
brandesmoore <Brandes.Moore20@gmail.com>
2020-02-07 20:52:07 -0600

    Question1 has been updated

--- Question1.sql
+++ Question1.sql
@@ -1,10 +1,29 @@
-/*Question 1*/
+/*
+Question 1: What range of years does the provided database cover?
 
-SELECT MIN(yearid) as min_year, MAX(yearid) AS max_year
-FROM batting;
+SOURCES ::
+       * go to the data dictionary
+	   * look in people table
+	   
+    
+	DIMENSIONS (columns) ::
+        * (people) playerid, debut, finalgame
+		  	
+  FACTS ::
+        Each player's debut and final game is in the people table
+		    
+	FILTERS ::      
+	* WHERE finalgame IS NOT NULL
+	
+	DESCRIPTION ::
+        I pulled the playerid, debut and finalgame columns 
+		ORDER BY finalgame DESC
+		The top row told me when the final game was and the last row told me when the earliest debut was
+		    
+	ANSWER :: The database covers years 1871-2017
+	*/
 
-SELECT MIN(yearid) as min_year, MAX(yearid) AS max_year
-FROM fielding;
-
-SELECT MIN(yearid) as min_year, MAX(yearid) AS max_year
-FROM pitching;
+SELECT playerid, debut, finalgame
+FROM people
+WHERE finalgame IS NOT NULL
+ORDER BY finalgame DESC;
